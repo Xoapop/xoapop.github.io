a3173edc57828158e3b588ca23d7a17ad964ac3b
Xoapop <149486414+Xoapop@users.noreply.github.com>
2023-10-31 23:26:23 -0400

    Add files via upload

---
+++ beta.js
@@ -0,0 +1,34 @@
+var mirrors = [
+    "https://edpuzzle.librecheats.net",
+    "https://edpuzzle.hs.vc",
+    "https://edpuzzle-beta.hs.vc"
+  ];
+  
+  async function try_mirror(mirror) {
+    let r = await fetch(mirror + "/app/main.js");
+    let script = await r.text();
+    window.base_url = mirror;
+    eval(script);
+  }
+  
+  async function init() {
+    if (window.location.hostname == "edpuzzle.hs.vc") {
+      alert("To use this, drag this button into your bookmarks bar. Then, run it when you're on an Edpuzzle assignment.");
+      return;
+    }
+    if (document.dev_env) {
+      return try_mirror(document.dev_env)
+    }
+  
+    for (let mirror of mirrors) {
+      try {
+        await try_mirror(mirror);
+        return;
+      }
+      catch {}
+    }
+    
+    alert("Error: Could not connect to any of the mirrors. Check that they're not blocked.")
+  }
+  
+  init();
